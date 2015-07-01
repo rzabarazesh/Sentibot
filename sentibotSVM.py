5155771d14b8e68ff701b6fc821d3ec6c64d71be
Mohammad Reza Brz <al73rna@gmail.com>
2015-07-01 11:16:22 +0430

    SVM REVISION 1

--- sentibotSVM.py
+++ sentibotSVM.py
@@ -1,4 +1,92 @@
 __author__ = 'mohammadreza'
+
+
+
+
+
+__author__ = 'mohammadreza'
+import re
+import csv
+import pickle
+_stopwordFileName = "stopwords.txt"
+
+def preProcess(tweet):
+    #to lower case
+    tweet = tweet.lower()
+    #remove links
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',tweet)
+    #remove @'s
+    tweet = re.sub('@[^\s]+','',tweet)
+    #remove additional white spaces
+    tweet = re.sub('[\s]+', ' ', tweet)
+    #Replace #tag with tag
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    tweet = tweet.strip('\'"')
+    return (tweet)
+
+def simplifyLongWords(w):
+    #simplify words with more than 2 recurring characters "salaaaaaaam" ,"salaaaam" , "salaaaaaaaaaam" ===> "salaam"
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return (pattern.sub(r"\1\1", w))
+def save_classifier(classifier):
+   f = open("savedClassifier", 'wb')
+   pickle.dump(classifier, f)
+   f.close()
+
+def load_classifier():
+   f = open("savedClassifier", 'rb')
+   classifier = pickle.load(f)
+   f.close()
+   return classifier
+
+def getStopWords():
+    stopwords = []
+    for line in open(_stopwordFileName).readlines():
+        stopwords.append(line.strip("\n"))
+    return (stopwords)
+stopwords = getStopWords()
+
+def getFeatures(tweet):
+    v = [] #feature vector
+    bagOfWords = tweet.split()
+    for word in bagOfWords :
+        word = simplifyLongWords(word)
+        word = word.strip('\'"?.,.')
+        startsWithAZ = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", word)
+        if(word in stopwords or startsWithAZ == None):
+            continue
+        else :
+            v.append(word.lower())
+    return v
+
+#### START OF PROGRAM
+print("......INITIATING THE SENTI BOT ....")
+print("- CODED BY MOHAMMAD REZA BARAZESH -")
+print("___________ AL73rnA _______________")
+####
+learningSet = csv.reader(open("learning.csv"))
+AllFeatures = []
+AllTweets = []
+
+def featureVector(tweet):
+    tweet_words = set(tweet)
+    features = {}
+    for word in AllFeatures:
+        features[word] = (word in tweet_words)
+    return features
+
+
+
+AllFeatures = list(set(AllFeatures))
+
+###
+
+
+
+
+
+
+
 def getSVMFeatureVectorAndLabels(tweets, featureList):
     sortedFeatures = sorted(featureList)
     map = {}
